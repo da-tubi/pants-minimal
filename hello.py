5bcc289f83f646a951c555a803273197ebb064eb
Da Shen <da@tubi.tv>
2022-11-24 17:56:14 +0800

    Test version

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
 import pandas
 
 def main():
+    try:
+        import pyspark
+        print(f"PySpark version {pyspark.__version__}")
+    except:
+        print("PySpark not available")
+
     print(f"Hello with Pandas {pandas.__version__}")
+
